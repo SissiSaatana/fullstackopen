c928f14ec675fabe27519be80fd33eb4ee53f07a
Juuso Laanti <juuso.laanti@gmail.com>
2023-10-25 21:31:26 +0300

    exercise 1.2 korjattu noudattamaan lamea xD tehtavanantoa, aakkosilla

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -7,7 +7,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <Part part={props.part} exercises={props.exercises} />
+    <>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+    </>
   )
 }
 
@@ -25,20 +29,19 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    'Fundamentals of React',
+    'Using props to pass data',
+    'State of a component'
+  ] 
 
+  const exercises = [10, 7, 14]
+  
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total totalExercises={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} exercises={exercises} />
+      <Total totalExercises={exercises.reduce((acc, val) => acc + val, 0 )} />
     </div>
   )
 }
