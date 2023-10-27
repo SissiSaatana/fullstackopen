ac646af32c5077b827155822e177bd3f58cc4f24
Juuso Laanti <juuso.laanti@gmail.com>
2023-10-27 19:00:03 +0300

    exercise 2.5 The monkeys have learned to get components for sepate files. This will help keeping the bananas well organized

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,38 +1,4 @@
-const Course = ({courses}) => 
-  <>
-    {courses.map(course =>                        
-      <Content key={course.id} parts={course.parts} header={course.name} />      
-    )}    
-  </>  
-
-
-
-const Header = ({courseName}) => <h1>{courseName}</h1>
-
-
-
-const Content = ({parts, header}) => 
-  <>
-    <Header courseName={header} />
-    {parts.map(part => 
-      <Part key={part.id} name={part.name} exercises={part.exercises}/>
-    )}
-    <Total parts={parts} />
-  </>
-
-
-
-const Part = ({name, exercises}) => <p>{name} {exercises}</p>
-
-
-
-const Total = ({parts}) => 
-  <b>
-    <p>Total of exercises {parts.reduce((acc, obj) => acc + obj.exercises, 0)}</p>
-  </b>
-
-
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
