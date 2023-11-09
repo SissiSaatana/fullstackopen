4a49eaae737206e8581dab52ed5222916c06af9b
SissiSaatana <juuso.laanti@gmail.com>
2023-11-09 21:13:20 +0200

    exercise 2.9 & 2.10 monkeys encountered nasty side effects!

--- osa2/puhelinluettelo-2/src/App.jsx
+++ osa2/puhelinluettelo-2/src/App.jsx
@@ -1,8 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const PersonForm = ({addName, newName, handleNameChange, newNumber, handleNumberChange}) => {
+  return (
+    <form onSubmit={addName}>
+      <div>
+        name: 
+        <input 
+          value={newName}
+          onChange={(e) => handleNameChange(e)}  
+        />
+      </div>
+      <div>
+        number:
+        <input
+          value={newNumber}
+          onChange={(e) => handleNumberChange(e)}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
 
 const DisplayContacts = ({contacts}) => {
-  console.log(contacts)
-  
+  console.log(contacts)  
   return (
     <>
       <h1>Numbers</h1>
@@ -13,12 +36,33 @@ const DisplayContacts = ({contacts}) => {
   )
 }
 
+const FilterPersons = ({handleFilterChange}) =>
+  <div>
+    filter shown with 
+    <input 
+      onChange={(e) => handleFilterChange(e.target.value)}
+    />
+  </div>
+
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },    
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [filteredPersons, setFilteredPersons] = useState(persons)
+  const [filterWithStr, setFilterWithStr] = useState('') 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect')
+    console.log(persons)
+    filterChange(filterWithStr)
+  })
+
 
   const addName = (event) => {
     event.preventDefault()
@@ -29,10 +73,11 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(newPerson))
+      
       setNewName('')
       setNewNumber('')
-    }
+      setPersons(persons.concat(newPerson))      
+    }    
   }
 
   const handleNameChange = (event) => {
@@ -43,33 +88,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const filterChange = (filterWith) => {
+    console.log('persons', persons)
+    const result = persons.filter((person) => 
+      person.name.toLowerCase().includes(filterWith.toLowerCase()))
+    setFilteredPersons(result)
+    setFilterWithStr(filterWith)
+  }
+
   const checkForDuplicates = () => {  
-    return (persons.findIndex(person => person.name == newName))
+    return (persons.findIndex(person => person.name.toLowerCase() == newName))
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: 
-          <input 
-            value={newName}
-            onChange={handleNameChange}  
-          />
-        </div>
-        <div>
-          number:
-          <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <DisplayContacts contacts={persons} />
+      <FilterPersons handleFilterChange={filterChange}/>
+      <PersonForm 
+        addName={addName}
+        handleNameChange={handleNameChange}
+        newName={newName}
+        handleNumberChange={handleNumberChange}
+        newNumber={newNumber}
+      />      
+      <DisplayContacts contacts={filteredPersons} />
     </div>
   )
 
