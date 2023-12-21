29b2d1596f024f813b97cccc99e514c324126e7e
Juuso Laanti <juuso.laanti@gmail.com>
2023-12-21 17:52:46 +0200

    Exercise  4.2 with error handler & unknown endpoint middleware

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -1,10 +1,13 @@
-const config = require('./utils/config')
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const {info, error } = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-const {info, error } = require('./utils/logger');
+
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -17,5 +20,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 
 module.exports = app
