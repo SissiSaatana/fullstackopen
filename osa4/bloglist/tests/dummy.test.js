bd06fd0649fafbb19936cba3608573dd33a6147e
Juuso Laanti <juuso.laanti@gmail.com>
2023-12-21 20:02:08 +0200

    Exercise 4.4 monkeys are testing banana blogs likes

--- osa4/bloglist/tests/dummy.test.js
+++ osa4/bloglist/tests/dummy.test.js
@@ -6,3 +6,39 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Mymy',
+      author: 'Jymy',
+      url: 'http://www.jymy.com',
+      likes: 99,
+      __v: 0
+    }
+  ]
+  test('when list has no entries', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes([listWithOneBlog[0]])
+    expect(result).toBe(listWithOneBlog[0].likes)
+  })
+
+  test('list total likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(104)
+  })
+
+})
--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -3,6 +3,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0)
+    return 0
+
+  const likes = blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
+  return likes
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
