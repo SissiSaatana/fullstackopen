45214082f88e6ef5db81b5f9b3ceff14f4cc82cb
Juuso Laanti <juuso.laanti@gmail.com>
2024-01-06 06:17:33 +0200

    exercises 6.3 & 6.4

---
+++ osa5/redux-anecdotes/src/components/Anecdote.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { postAnecdote } from '../reducers/anecdoteReducer'
+
+const AddAnecdote = () => {
+  const dispatch = useDispatch()
+
+  const AddAnecdote = (e) => {
+    e.preventDefault()
+    const anecdote = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch(postAnecdote(anecdote))
+  }
+
+  return (
+    <form onSubmit={AddAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">add</button>
+    </form>
+  )
+}
+
+export default AddAnecdote
