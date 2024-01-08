a0b9af5fa69a0ad2e49efff989bcfb542b9c9124
Juuso Laanti <juuso.laanti@gmail.com>
2024-01-08 16:09:56 +0200

    exercise 7.7

---
+++ osa6/country-hook/src/hooks/index.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export const useCountry = (searchParam) => {  
+  // const [value, setValue] = useState('')
+
+  // const onChange = (event) => {
+  //   setValue(event.target.value)
+  // }
+
+  // const reset = (e) => {
+  //   e.preventDefault()
+  //   setValue('')
+  // }
+
+  return
+}
